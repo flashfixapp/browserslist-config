ff2f4006bfeb81af9f2a818b760146e331e21999
Max Degterev <max@degterev.me>
2022-04-01 16:35:02 +0200

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = [
+  'last 2 major versions',
+  'last 1 year',
+  '> 1%',
+  'not dead',
+  'not op_mini all',
+  'not op_mob > 0',
+  'not ie > 0',
+  'not ie_mob > 0',
+  'not kaios > 0',
+  'not baidu > 0',
+];
